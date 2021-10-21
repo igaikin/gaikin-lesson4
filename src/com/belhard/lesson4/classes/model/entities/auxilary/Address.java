cf311aa8f7a1bdcac07e13469f0c97c2bed97fe3
Herman <Herman2lv@gmail.com>
2021-10-21 20:43:50 +0300

    Refactor Address

--- src/com/belhard/lesson4/classes/model/entities/auxilary/Address.java
+++ src/com/belhard/lesson4/classes/model/entities/auxilary/Address.java
@@ -1,30 +1,103 @@
 package com.belhard.lesson4.classes.model.entities.auxilary;
 
-import com.belhard.lesson4.classes.model.entities.Teacher;
-
 public class Address {
 
-    String city;
-    String street;
-    int house;
-    String block;
-    int flat;
+    private Country country;
+    private String city;
+    private String street;
+    private int house;
+    private String block;
+    private int flat;
 
-    enum Country {
-        BLR("Republic of Belarus"), POL("Poland"), LTU("Lithuania"), LVA("Latvia"), DEU("Germany"),
-        RUS("Russian Federation"), UKR("Ukraine");
+    public Address(Country country, String city, String street, int house) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+    }
 
-        private String country;
+    public Address(Country country, String city, String street, int house, String block) {
+        this(country, city, street, house);
+        this.block = block;
+    }
 
-        Country(String country) {
+    public Address(Country country, String city, String street, int house, int flat) {
+        this(country, city, street, house);
+        this.flat = flat;
+    }
+
+    public Address(Country country, String city, String street, int house, String block, int flat) {
+        this(country, city, street, house, block);
+        this.flat = flat;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public int getFlat() {
+        return flat;
+    }
+
+    public void setFlat(int flat) {
+        this.flat = flat;
+    }
+
+    public enum Country {
+        BLR("Republic of Belarus"),
+        POL("Poland"),
+        LTU("Lithuania"),
+        LVA("Latvia"),
+        DEU("Germany"),
+        RUS("Russian Federation"),
+        UKR("Ukraine");
+
+        private final String name;
+
+        Country(String name) {
+            this.name = name;
         }
 
-        String getCountry() {
-
-            return country;
+        String getName() {
+            return name;
         }
 
     }
 
-}
 }
